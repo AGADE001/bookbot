bed3bf6f63590c3111bb7eb7ef4039827324d1c7
AGADE001 <agadeaariel@gmail.com>
2025-02-21 18:08:28 -0500

    added a word counter feature with word_counter function

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def word_counter(book_text):
+    words = book_text.split()
+    
+    word_count = len(words)
+
+    return word_count
+    
         
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    
+    word_count = word_counter(book_text)
+    print(f"{word_count} words found in the document")
 main()
