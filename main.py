1cff7f4fc92c255b7bcf2b63ebf7bfee79f4fb25
AGADE001 <agadeaariel@gmail.com>
2025-02-22 13:58:53 -0500

    added sort_key and sort_output function to manipulate character dictionary

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from stats import word_counter, character_counter
+from stats import word_counter, character_counter, sort_output
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def header_output(filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    filepath = "books/frankenstein.txt"
+    header_output(filepath)
+    book_text = get_book_text(filepath)
     word_count = word_counter(book_text)
-    print(f"{word_count} words found in the document")
+    print(f"Found {word_count} total words")
     
     character_dict = character_counter(book_text)
-    for key, value in character_dict.items():
-        print(f"'{key}': {value}")
+
+    output_list = sort_output(character_dict)
+
+    print("--------- Character Count -------")
+    for character in output_list:
+        for key, value in character.items():
+            if key.isalpha():
+                print(f"{key}: {value}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -12,4 +12,18 @@ def character_counter(book_text):
         else:
             all_characters[char] = 1
     return all_characters
+
+def sort_key(char_dict):
+    for key in char_dict:
+        return char_dict[key] 
+
+def sort_output(char_dict):
+    char_list = []
+
+    for key, value in char_dict.items():
+        new_dict = {key: value}
+        char_list.append(new_dict)
+
+    char_list.sort(key=sort_key, reverse=True)
+    return char_list
     
